a3be741b2777de8885dbd45d494ee525665db031
fwanjai <fwanjai@gmail.com>
2017-12-13 13:46:47 +0300

    creating a palyer object and setting all values to zero

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,12 @@ var player2="";
 var throwdice = function () {
   return Math.floor(6*Math.random())+1;
 }
+
+//Creating a player object and setting all counts to initial count 0
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
