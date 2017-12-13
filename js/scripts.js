3cd63a42a2b2a2501f197fe80deb8cd0d8f1e87d
fwanjai <fwanjai@gmail.com>
2017-12-13 13:58:11 +0300

    setting a function to show the playing tab and hide the forms

--- js/scripts.js
+++ js/scripts.js
@@ -63,4 +63,13 @@ $(document).ready(function() {
     $(".player-console").show();
     $(".start-menu").hide();
 
-    
+    var player1Name = $(".player1Name").val();
+    $("#player1Name").text(player1Name);
+
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
+
+    player1.playerName=player1Name;
+    player2.playerName=player2Name;
+
+  });
